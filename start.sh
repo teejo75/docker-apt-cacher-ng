33ab76df25320ba7c3cf51c950f0492ceb42b96c
teejo75 <437200+teejo75@users.noreply.github.com>
2023-06-01 21:53:44 +1000

    Allow apt-cacher-ng to background & tail logs

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 if [ ! -f /etc/apt-cacher-ng/acng.conf ]; then
     cp -a /tmp/acng/* /etc/apt-cacher-ng
 fi
-/usr/sbin/apt-cacher-ng -c /etc/apt-cacher-ng ForeGround=1
+/usr/sbin/apt-cacher-ng -c /etc/apt-cacher-ng ForeGround=0
+tail -f /var/log/apt-cacher-ng/apt-cacher.err /var/log/apt-cacher-ng/apt-cacher.log
